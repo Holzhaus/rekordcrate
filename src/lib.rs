604f93d3a035de89e3787a6d4aa46d926339b559
Jan Holthuis <jan.holthuis@ruhr-uni-bochum.de>
2022-02-02 18:19:07 +0100

    lib.rs: Add linter settings and remove sample code

--- src/lib.rs
+++ src/lib.rs
@@ -6,10 +6,19 @@
 //
 // SPDX-License-Identifier: MPL-2.0
 
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
-}
+//! This library provides access to device libraries exported from Pioneer's Rekordbox DJ software.
+
+#![warn(unsafe_code)]
+#![cfg_attr(not(debug_assertions), deny(warnings))]
+#![deny(rust_2018_idioms)]
+#![deny(rust_2021_compatibility)]
+#![deny(missing_debug_implementations)]
+#![deny(missing_docs)]
+#![deny(rustdoc::broken_intra_doc_links)]
+#![deny(clippy::all)]
+#![deny(clippy::explicit_deref_methods)]
+#![deny(clippy::explicit_into_iter_loop)]
+#![deny(clippy::explicit_iter_loop)]
+#![deny(clippy::must_use_candidate)]
+#![cfg_attr(not(test), deny(clippy::panic_in_result_fn))]
+#![cfg_attr(not(debug_assertions), deny(clippy::used_underscore_binding))]
