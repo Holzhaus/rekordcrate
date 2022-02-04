040aa7aedfa2e38fd77d583013e06b1e0e4a3e94
Jan Holthuis <jan.holthuis@ruhr-uni-bochum.de>
2022-02-04 12:12:46 +0100

    main: Improve output using indentation to reflect the file structure

--- src/main.rs
+++ src/main.rs
@@ -15,20 +15,23 @@ fn main() {
 
     println!("{:#?}", header);
 
-    for table in &header.tables {
+    for (i, table) in header.tables.iter().enumerate() {
+        println!("Table {}: {:?}", i, table.page_type);
         for page_index in table.page_indices() {
             let (_, page) = header
                 .page(&data, &page_index)
                 .expect("failed to parse page");
+            println!("  {:?}", page);
             assert_eq!(page.page_index, page_index);
             let page_offset = header.page_offset(&page_index).unwrap();
             let page_data = &data[page_offset..];
             page.row_groups(page_data, header.page_size)
                 .for_each(|row_group| {
+                    println!("    {:?}", row_group);
                     let RowGroup(row_offsets) = row_group;
                     for row_offset in row_offsets {
                         let (_, row) = page.row(page_data, &row_offset).unwrap();
-                        println!("{:?}", row);
+                        println!("      {:?}", row);
                     }
                 })
         }
