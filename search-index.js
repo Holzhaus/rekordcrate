a88899ce04b73ae67d2486f7f7041781d039a7da
Holzhaus <Holzhaus@users.noreply.github.com>
2022-02-06 10:20:41 +0000

    deploy: bb402415df08e522ebb9ce24b627d157393ba908

---
+++ search-index.js
@@ -0,0 +1,4 @@
+var searchIndex = JSON.parse('{\
+"rekordcrate":{"doc":"This library provides access to device libraries exported …","t":[],"n":[],"q":[],"d":[],"i":[],"f":[],"p":[]}\
+}');
+if (window.initSearch) {window.initSearch(searchIndex)};
---
+++ source-files.js
@@ -0,0 +1,3 @@
+var N = null;var sourcesIndex = {};
+sourcesIndex["rekordcrate"] = {"name":"","files":["lib.rs"]};
+createSourceSidebar();
