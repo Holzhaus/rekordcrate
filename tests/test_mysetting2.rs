d2fee55113c9302c549a873be28b15259155ea3c
Jan Holthuis <jan.holthuis@ruhr-uni-bochum.de>
2022-02-24 12:17:23 +0100

    tests: Add tests for individual `MYSETTING2.DAT` options

---
+++ tests/test_mysetting2.rs
@@ -0,0 +1,100 @@
+use binrw::{io::Cursor, BinRead};
+use rekordcrate::setting::*;
+
+macro_rules! read_mysetting2 {
+    ($path:literal) => {{
+        let data = include_bytes!($path);
+        println!("Setting file: {}", $path);
+        let mut reader = Cursor::new(data);
+        let setting = Setting::read(&mut reader).expect("failed to parse setting file");
+        let result = match setting.data {
+            SettingData::MySetting2(x) => Some(x),
+            _ => None,
+        };
+        result.expect("failed to match data section")
+    }};
+}
+
+#[test]
+fn read_mysetting2_beatjumpbeatvalue_64() {
+    let data = read_mysetting2!("../data/mysetting2/beatjumpbeatvalue_64/MYSETTING2.DAT");
+    assert_eq!(data.beat_jump_beat_value, BeatJumpBeatValue::SixtyfourBeat);
+}
+
+#[test]
+fn read_mysetting2_beatjumpbeatvalue_half() {
+    let data = read_mysetting2!("../data/mysetting2/beatjumpbeatvalue_half/MYSETTING2.DAT");
+    assert_eq!(data.beat_jump_beat_value, BeatJumpBeatValue::HalfBeat);
+}
+
+#[test]
+fn read_mysetting2_beatjumpbeatvalue_one() {
+    let data = read_mysetting2!("../data/mysetting2/beatjumpbeatvalue_one/MYSETTING2.DAT");
+    assert_eq!(data.beat_jump_beat_value, BeatJumpBeatValue::OneBeat);
+}
+
+#[test]
+fn read_mysetting2_jogdisplaymode_artwork() {
+    let data = read_mysetting2!("../data/mysetting2/jogdisplaymode_artwork/MYSETTING2.DAT");
+    assert_eq!(data.jog_display_mode, JogDisplayMode::Artwork);
+}
+
+#[test]
+fn read_mysetting2_jogdisplaymode_info() {
+    let data = read_mysetting2!("../data/mysetting2/jogdisplaymode_info/MYSETTING2.DAT");
+    assert_eq!(data.jog_display_mode, JogDisplayMode::Info);
+}
+
+#[test]
+fn read_mysetting2_jogdisplaymode_simple() {
+    let data = read_mysetting2!("../data/mysetting2/jogdisplaymode_simple/MYSETTING2.DAT");
+    assert_eq!(data.jog_display_mode, JogDisplayMode::Simple);
+}
+
+#[test]
+fn read_mysetting2_joglcdbrightness_1() {
+    let data = read_mysetting2!("../data/mysetting2/joglcdbrightness_1/MYSETTING2.DAT");
+    assert_eq!(data.jog_lcd_brightness, JogLCDBrightness::One);
+}
+
+#[test]
+fn read_mysetting2_joglcdbrightness_5() {
+    let data = read_mysetting2!("../data/mysetting2/joglcdbrightness_5/MYSETTING2.DAT");
+    assert_eq!(data.jog_lcd_brightness, JogLCDBrightness::Five);
+}
+
+#[test]
+fn read_mysetting2_padbuttonbrightness_1() {
+    let data = read_mysetting2!("../data/mysetting2/padbuttonbrightness_1/MYSETTING2.DAT");
+    assert_eq!(data.pad_button_brightness, PadButtonBrightness::One);
+}
+
+#[test]
+fn read_mysetting2_padbuttonbrightness_5() {
+    let data = read_mysetting2!("../data/mysetting2/padbuttonbrightness_4/MYSETTING2.DAT");
+    assert_eq!(data.pad_button_brightness, PadButtonBrightness::Four);
+}
+
+#[test]
+fn read_mysetting2_vinylspeedadjust_release() {
+    let data = read_mysetting2!("../data/mysetting2/vinylspeedadjust_release/MYSETTING2.DAT");
+    assert_eq!(data.vinyl_speed_adjust, VinylSpeedAdjust::Release);
+}
+
+#[test]
+fn read_mysetting2_vinylspeedadjust_touchrelease() {
+    let data = read_mysetting2!("../data/mysetting2/vinylspeedadjust_touchrelease/MYSETTING2.DAT");
+    assert_eq!(data.vinyl_speed_adjust, VinylSpeedAdjust::TouchRelease);
+}
+
+#[test]
+fn read_mysetting2_waveformdivisions_timescale() {
+    let data = read_mysetting2!("../data/mysetting2/waveformdivisions_timescale/MYSETTING2.DAT");
+    assert_eq!(data.waveform_divisions, WaveformDivisions::TimeScale);
+}
+
+#[test]
+fn read_mysetting2_waveform_phasemeter() {
+    let data = read_mysetting2!("../data/mysetting2/waveform_phasemeter/MYSETTING2.DAT");
+    assert_eq!(data.waveform, Waveform::PhaseMeter);
+}
