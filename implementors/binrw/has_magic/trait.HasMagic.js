ebeefe30c11de7c2efe5955d09856f719ccea14a
Holzhaus <Holzhaus@users.noreply.github.com>
2022-09-29 16:40:46 +0000

    deploy: 2b8b55964b54130d32eb2f9bd4d3dc7a968a95be

--- implementors/binrw/has_magic/trait.HasMagic.js
+++
@@ -1,3 +0,0 @@
-(function() {var implementors = {};
-implementors["rekordcrate"] = [{"text":"impl HasMagic for <a class=\"struct\" href=\"rekordcrate/pdb/struct.Page.html\" title=\"struct rekordcrate::pdb::Page\">Page</a>","synthetic":false,"types":["rekordcrate::pdb::Page"]}];
-if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
