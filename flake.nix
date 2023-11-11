ebefd8dbcb83f56c343a807b7318ae0c9b139cd3
Kerstin Humm <kerstin@erictapen.name>
2023-11-11 15:49:03 +0100

    nixpkgs-fmt

--- flake.nix
+++ flake.nix
@@ -3,14 +3,18 @@
 
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
 
-  outputs = { self, nixpkgs }: 
-  let
-    pkgs = import nixpkgs { system = "x86_64-linux"; };
-  in {
+  outputs = { self, nixpkgs }:
+    let
+      pkgs = import nixpkgs { system = "x86_64-linux"; };
+    in
+    {
 
-    devShells.x86_64-linux.default = pkgs.mkShell {
-      buildInputs = with pkgs; [ cargo rustc ];
+      devShells.x86_64-linux.default = pkgs.mkShell {
+        buildInputs = with pkgs; [
+          cargo
+          rustc
+        ];
+      };
+
     };
-
-  };
 }
