fed774f3e217d75ff64bb65ad3e674172b9544dd
Holzhaus <Holzhaus@users.noreply.github.com>
2025-02-25 00:21:39 +0000

    deploy: 92d7de94f59dbda1e68c2fa6ebf35b8c3c8ecd95

--- trait.impl/serde/de/trait.Deserialize.js
+++ trait.impl/serde/de/trait.Deserialize.js
@@ -1,5 +1,5 @@
 (function() {
-    var implementors = Object.fromEntries([["rekordcrate",[["impl&lt;'de&gt; <a class=\"trait\" href=\"https://docs.rs/serde/1.0.217/serde/de/trait.Deserialize.html\" title=\"trait serde::de::Deserialize\">Deserialize</a>&lt;'de&gt; for <a class=\"struct\" href=\"rekordcrate/xml/struct.Document.html\" title=\"struct rekordcrate::xml::Document\">Document</a>"]]]]);
+    var implementors = Object.fromEntries([["rekordcrate",[["impl&lt;'de&gt; <a class=\"trait\" href=\"https://docs.rs/serde/1.0.218/serde/de/trait.Deserialize.html\" title=\"trait serde::de::Deserialize\">Deserialize</a>&lt;'de&gt; for <a class=\"struct\" href=\"rekordcrate/xml/struct.Document.html\" title=\"struct rekordcrate::xml::Document\">Document</a>"]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
