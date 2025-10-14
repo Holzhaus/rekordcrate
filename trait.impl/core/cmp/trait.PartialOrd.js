032b6093f059491f01629fc27b8c95829b4ba1ae
Holzhaus <Holzhaus@users.noreply.github.com>
2025-10-14 22:15:09 +0000

    deploy: 0f4747af40ceead891025d3cb4913b7cdcb5e23f

--- trait.impl/core/cmp/trait.PartialOrd.js
+++ trait.impl/core/cmp/trait.PartialOrd.js
@@ -1,5 +1,5 @@
 (function() {
-    var implementors = Object.fromEntries([["rekordcrate",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.89.0/core/cmp/trait.PartialOrd.html\" title=\"trait core::cmp::PartialOrd\">PartialOrd</a> for <a class=\"struct\" href=\"rekordcrate/pdb/struct.PageIndex.html\" title=\"struct rekordcrate::pdb::PageIndex\">PageIndex</a>"]]]]);
+    var implementors = Object.fromEntries([["rekordcrate",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.90.0/core/cmp/trait.PartialOrd.html\" title=\"trait core::cmp::PartialOrd\">PartialOrd</a> for <a class=\"struct\" href=\"rekordcrate/pdb/struct.PageIndex.html\" title=\"struct rekordcrate::pdb::PageIndex\">PageIndex</a>"]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
