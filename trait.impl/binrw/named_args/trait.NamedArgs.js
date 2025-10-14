a5bcc573185680501f904e1496134e998e43c631
Holzhaus <Holzhaus@users.noreply.github.com>
2025-10-14 22:23:36 +0000

    deploy: e31b7490658f9407104023d0948bc207288a3738

---
+++ trait.impl/binrw/named_args/trait.NamedArgs.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["rekordcrate",[["impl NamedArgs for <a class=\"struct\" href=\"rekordcrate/pdb/struct.DataPageContentBinReadArgs.html\" title=\"struct rekordcrate::pdb::DataPageContentBinReadArgs\">DataPageContentBinReadArgs</a>"],["impl NamedArgs for <a class=\"struct\" href=\"rekordcrate/pdb/struct.PageContentBinReadArgs.html\" title=\"struct rekordcrate::pdb::PageContentBinReadArgs\">PageContentBinReadArgs</a>"],["impl NamedArgs for <a class=\"struct\" href=\"rekordcrate/pdb/struct.PageContentBinWriteArgs.html\" title=\"struct rekordcrate::pdb::PageContentBinWriteArgs\">PageContentBinWriteArgs</a>"]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[596]}
---
+++ trait.impl/core/convert/trait.TryFrom.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["rekordcrate",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.90.0/core/convert/trait.TryFrom.html\" title=\"trait core::convert::TryFrom\">TryFrom</a>&lt;(<a class=\"struct\" href=\"rekordcrate/pdb/struct.PageIndex.html\" title=\"struct rekordcrate::pdb::PageIndex\">PageIndex</a>, <a class=\"primitive\" href=\"https://doc.rust-lang.org/1.90.0/std/primitive.u8.html\">u8</a>)&gt; for <a class=\"struct\" href=\"rekordcrate/pdb/struct.IndexEntry.html\" title=\"struct rekordcrate::pdb::IndexEntry\">IndexEntry</a>"],["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.90.0/core/convert/trait.TryFrom.html\" title=\"trait core::convert::TryFrom\">TryFrom</a>&lt;<a class=\"primitive\" href=\"https://doc.rust-lang.org/1.90.0/std/primitive.u32.html\">u32</a>&gt; for <a class=\"struct\" href=\"rekordcrate/pdb/struct.PageIndex.html\" title=\"struct rekordcrate::pdb::PageIndex\">PageIndex</a>"]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[919]}
