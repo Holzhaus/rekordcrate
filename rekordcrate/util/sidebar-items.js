fd16560bfca169addfacaf5a5290a42a051c43e6
Swiftb0y <Swiftb0y@users.noreply.github.com>
2025-10-28 11:38:41 +0000

    deploy: 280d47071bb638b14238439ec8143e30aa19bb11

--- rekordcrate/util/sidebar-items.js
+++ rekordcrate/util/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"enum":["ColorIndex","RekordcrateError"],"fn":["align_by"],"struct":["ExplicitPadding"],"type":["RekordcrateResult"]};
+window.SIDEBAR_ITEMS = {"enum":["ColorIndex","FileType","RekordcrateError"],"fn":["align_by"],"struct":["ExplicitPadding"],"type":["RekordcrateResult"]};
