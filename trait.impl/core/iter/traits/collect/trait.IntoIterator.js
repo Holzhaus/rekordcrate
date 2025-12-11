d6f0417899c66c2022ae1f900fa65defbef89ffe
Swiftb0y <Swiftb0y@users.noreply.github.com>
2025-12-11 19:50:19 +0000

    deploy: d421bd805a036f3b7de18257b9f056c69db42721

---
+++ trait.impl/core/iter/traits/collect/trait.IntoIterator.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["rekordcrate",[["impl&lt;'a&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/1.91.1/core/iter/traits/collect/trait.IntoIterator.html\" title=\"trait core::iter::traits::collect::IntoIterator\">IntoIterator</a> for &amp;'a <a class=\"struct\" href=\"rekordcrate/pdb/struct.RowGroup.html\" title=\"struct rekordcrate::pdb::RowGroup\">RowGroup</a>"],["impl&lt;'a&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/1.91.1/core/iter/traits/collect/trait.IntoIterator.html\" title=\"trait core::iter::traits::collect::IntoIterator\">IntoIterator</a> for &amp;'a mut <a class=\"struct\" href=\"rekordcrate/pdb/struct.RowGroup.html\" title=\"struct rekordcrate::pdb::RowGroup\">RowGroup</a>"]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[699]}
---
+++ trait.impl/core/ops/deref/trait.DerefMut.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["rekordcrate",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.91.1/core/ops/deref/trait.DerefMut.html\" title=\"trait core::ops::deref::DerefMut\">DerefMut</a> for <a class=\"struct\" href=\"rekordcrate/pdb/struct.RowGroup.html\" title=\"struct rekordcrate::pdb::RowGroup\">RowGroup</a>"]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[305]}
