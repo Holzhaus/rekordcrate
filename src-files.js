46771c084792948e4e24601a056e8dd2ab5ffb3a
Swiftb0y <Swiftb0y@users.noreply.github.com>
2025-12-13 14:02:50 +0000

    deploy: 38dc3a94602c1fe3edce0f5521c23e5746d97abc

--- src-files.js
+++ src-files.js
@@ -1,2 +1,2 @@
-createSrcSidebar('[["rekordcrate",["",[["pdb",[],["ext.rs","mod.rs","offset_array.rs","string.rs"]]],["anlz.rs","lib.rs","setting.rs","util.rs","xml.rs","xor.rs"]]]]');
-//{"start":19,"fragment_lengths":[145]}
+createSrcSidebar('[["rekordcrate",["",[["pdb",[],["bitfields.rs","ext.rs","mod.rs","offset_array.rs","string.rs"]]],["anlz.rs","lib.rs","setting.rs","util.rs","xml.rs","xor.rs"]]]]');
+//{"start":19,"fragment_lengths":[160]}
--- trait.impl/modular_bitfield/private/checks/trait.CheckTotalSizeMultipleOf8.js
+++ trait.impl/modular_bitfield/private/checks/trait.CheckTotalSizeMultipleOf8.js
@@ -1,9 +1,9 @@
 (function() {
-    var implementors = Object.fromEntries([["rekordcrate",[["impl CheckTotalSizeMultipleOf8 for <a class=\"struct\" href=\"rekordcrate/anlz/struct.TinyWaveformPreviewColumn.html\" title=\"struct rekordcrate::anlz::TinyWaveformPreviewColumn\">TinyWaveformPreviewColumn</a>"],["impl CheckTotalSizeMultipleOf8 for <a class=\"struct\" href=\"rekordcrate/anlz/struct.WaveformColorDetailColumn.html\" title=\"struct rekordcrate::anlz::WaveformColorDetailColumn\">WaveformColorDetailColumn</a>"],["impl CheckTotalSizeMultipleOf8 for <a class=\"struct\" href=\"rekordcrate/anlz/struct.WaveformPreviewColumn.html\" title=\"struct rekordcrate::anlz::WaveformPreviewColumn\">WaveformPreviewColumn</a>"]]]]);
+    var implementors = Object.fromEntries([["rekordcrate",[["impl CheckTotalSizeMultipleOf8 for <a class=\"struct\" href=\"rekordcrate/anlz/struct.TinyWaveformPreviewColumn.html\" title=\"struct rekordcrate::anlz::TinyWaveformPreviewColumn\">TinyWaveformPreviewColumn</a>"],["impl CheckTotalSizeMultipleOf8 for <a class=\"struct\" href=\"rekordcrate/anlz/struct.WaveformColorDetailColumn.html\" title=\"struct rekordcrate::anlz::WaveformColorDetailColumn\">WaveformColorDetailColumn</a>"],["impl CheckTotalSizeMultipleOf8 for <a class=\"struct\" href=\"rekordcrate/anlz/struct.WaveformPreviewColumn.html\" title=\"struct rekordcrate::anlz::WaveformPreviewColumn\">WaveformPreviewColumn</a>"],["impl CheckTotalSizeMultipleOf8 for <a class=\"struct\" href=\"rekordcrate/pdb/bitfields/struct.PackedRowCounts.html\" title=\"struct rekordcrate::pdb::bitfields::PackedRowCounts\">PackedRowCounts</a>"]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
         window.pending_implementors = implementors;
     }
 })()
-//{"start":57,"fragment_lengths":[650]}
+//{"start":57,"fragment_lengths":[854]}
