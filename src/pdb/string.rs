4ab3bf8f2722df740b936026d66922858c017319
Swiftb0y <12380386+Swiftb0y@users.noreply.github.com>
2026-01-16 15:41:31 +0100

    chore: update copyright year

--- src/pdb/string.rs
+++ src/pdb/string.rs
@@ -1,4 +1,4 @@
-// Copyright (c) 2025 Nikolaus Einhauser
+// Copyright (c) 2026 Nikolaus Einhauser
 //
 // This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0. If a copy
 // of the MPL was not distributed with this file, You can obtain one at
