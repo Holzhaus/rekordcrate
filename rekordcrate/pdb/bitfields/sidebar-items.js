1cd948efa677f2b54a6f6ada53542f9acdbf75b2
Swiftb0y <Swiftb0y@users.noreply.github.com>
2026-03-11 15:37:47 +0000

    deploy: 538b15fc4dc1534e54df96f20676dc59ac105ad6

--- rekordcrate/pdb/bitfields/sidebar-items.js
+++ rekordcrate/pdb/bitfields/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"struct":["PackedRowCounts"]};
+window.SIDEBAR_ITEMS = {"struct":["PackedRowCounts","PageFlags"]};
---
+++ trait.impl/modular_bitfield/private/checks/trait.CheckFillsUnalignedBits.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["rekordcrate",[["impl CheckFillsUnalignedBits for <a class=\"struct\" href=\"rekordcrate/pdb/bitfields/struct.PageFlags.html\" title=\"struct rekordcrate::pdb::bitfields::PageFlags\">PageFlags</a>"]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[201]}
