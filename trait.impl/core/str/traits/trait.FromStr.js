24003076be61c0fffe1eff7ac251576ecb990fac
Holzhaus <Holzhaus@users.noreply.github.com>
2026-04-21 05:29:36 +0000

    deploy: ef14d41349def72d7f2c6f8c0af5a648c430ea12

--- trait.impl/core/str/traits/trait.FromStr.js
+++ trait.impl/core/str/traits/trait.FromStr.js
@@ -1,5 +1,5 @@
 (function() {
-    const implementors = Object.fromEntries([["rekordcrate",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.94.1/core/str/traits/trait.FromStr.html\" title=\"trait core::str::traits::FromStr\">FromStr</a> for <a class=\"struct\" href=\"rekordcrate/pdb/string/struct.DeviceSQLString.html\" title=\"struct rekordcrate::pdb::string::DeviceSQLString\">DeviceSQLString</a>",0]]]]);
+    const implementors = Object.fromEntries([["rekordcrate",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.95.0/core/str/traits/trait.FromStr.html\" title=\"trait core::str::traits::FromStr\">FromStr</a> for <a class=\"struct\" href=\"rekordcrate/pdb/string/struct.DeviceSQLString.html\" title=\"struct rekordcrate::pdb::string::DeviceSQLString\">DeviceSQLString</a>",0]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
