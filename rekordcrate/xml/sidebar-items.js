65c84cd9e8ba1e7cc80c3411cb14b2523c1cac34
Holzhaus <Holzhaus@users.noreply.github.com>
2026-04-21 05:32:10 +0000

    deploy: 12083c24696bb3b597f2ff97bacc22b03f37422a

--- rekordcrate/xml/sidebar-items.js
+++ rekordcrate/xml/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"struct":["Document"]};
+window.SIDEBAR_ITEMS = {"enum":["PlaylistGenericNode","StarRating"],"struct":["Collection","Document","PlaylistFolderNode","PlaylistPlaylistNode","PlaylistTrack","Playlists","PositionMark","Product","Tempo","Track"]};
