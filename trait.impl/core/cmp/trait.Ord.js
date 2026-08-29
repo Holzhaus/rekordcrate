2f35114647e50269b00b12b26ac59b19f97f64f9
acrilique <acrilique@users.noreply.github.com>
2026-08-29 14:53:09 +0000

    deploy: 41f790b2692d73696c492b98fbf8ebb61f439103

--- trait.impl/core/cmp/trait.Ord.js
+++ trait.impl/core/cmp/trait.Ord.js
@@ -1,5 +1,5 @@
 (function() {
-    const implementors = Object.fromEntries([["rekordcrate",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.96.1/core/cmp/trait.Ord.html\" title=\"trait core::cmp::Ord\">Ord</a> for <a class=\"struct\" href=\"rekordcrate/util/struct.TableIndex.html\" title=\"struct rekordcrate::util::TableIndex\">TableIndex</a>",0]]]]);
+    const implementors = Object.fromEntries([["rekordcrate",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.98.0/core/cmp/trait.Ord.html\" title=\"trait core::cmp::Ord\">Ord</a> for <a class=\"struct\" href=\"rekordcrate/util/struct.TableIndex.html\" title=\"struct rekordcrate::util::TableIndex\">TableIndex</a>",0]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
